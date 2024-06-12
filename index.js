45658c9e2dd734e975c956ce83a59eaf5a341562
codeCampWithSifat <sayedhossainsifat300@.com>
2024-06-12 12:43:56 +0600

    setup the project

---
+++ index.js
@@ -0,0 +1,33 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7aech.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Ticket Counter App");
+});
+
+app.listen(port, () => {
+  console.log(`Listening To The ${port} Successfully`);
+});
